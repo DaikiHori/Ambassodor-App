e6fbdbb3d8a9fee472862c461aae3463e4e545a4
daiki.hori <daiki.hori@shine-spark.co.jp>
2024-06-12 00:34:32 +0900

    完全に理解した

--- app/src/main/java/com/example/inventory/ui/codes/CodesScreen.kt
+++ app/src/main/java/com/example/inventory/ui/codes/CodesScreen.kt
@@ -2,13 +2,18 @@ package com.example.inventory.ui.codes
 
 import android.graphics.Bitmap
 import android.util.Log
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +25,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
@@ -90,7 +96,8 @@ private fun CodesBody(
         CodesList(
             eventList = eventList,
             contentPadding = contentPadding,
-            modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            modifier = Modifier
+                .padding(horizontal = dimensionResource(id = R.dimen.padding_small))
         )
     }
 }
@@ -102,7 +109,7 @@ private fun CodesList(
 ) {
     LazyRow(
         modifier = modifier,
-        contentPadding = contentPadding
+        contentPadding = contentPadding,
     ) {
         items(items = eventList, key = { it.event.id }) { event ->
             for(e in event.codes) {
@@ -110,6 +117,7 @@ private fun CodesList(
                     code = e,
                     modifier = Modifier
                         .padding(dimensionResource(id = R.dimen.padding_small))
+                        .fillParentMaxWidth()
                 )
             }
         }
@@ -161,8 +169,8 @@ fun createBitMatrix(data: String): BitMatrix? {
     return multiFormatWriter.encode(
         data,
         BarcodeFormat.QR_CODE,
-        500,
-        500,
+        700,
+        700,
         hints
     )
 }
