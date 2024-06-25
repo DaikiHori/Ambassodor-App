280970640ce697b2b5b69efb3ac26638f845eb84
daiki.hori <daiki.hori@shine-spark.co.jp>
2024-06-25 23:20:57 +0900

    コード一覧画面　とりあえず表示出来ただけ

--- app/src/main/java/com/cambassador/app/ui/codes_in_event/CodesDetailScreen.kt
+++ app/src/main/java/com/cambassador/app/ui/codes_in_event/CodesDetailScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,7 +23,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -81,10 +84,9 @@ fun CodesBody(
     modifier: Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp)
 ){
-    Box(modifier = Modifier.padding(contentPadding)) {
-        Box(modifier = modifier) {
-            Text(text = event.name, modifier = modifier.padding(5.dp))
-        }
+    Column(modifier = Modifier.padding(contentPadding)) {
+        Text(text = event.name, modifier = Modifier.padding(5.dp).height(25.dp))
+        Divider(color = Color.Gray)
         LazyColumn(
             modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small)),
             contentPadding = PaddingValues(5.dp,50.dp,5.dp,5.dp)
