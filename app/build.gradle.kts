fce9715ada23bc4f98481719b52b784eea02192b
daiki.hori <daiki.hori@shine-spark.co.jp>
2024-08-04 01:52:36 +0900

    リリース1.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.cambassador.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.11"
+        versionCode = 14
+        versionName = "1.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
